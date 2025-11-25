b2b71a3b4248b24c3b68164a8fc515414baac8cd
fellon-dnb <fellon@ancevt.com>
2025-11-25 15:15:28 +0300

    init:  verification of GeometryLibrary v1.0.0 (area & perimeter calculations)

---
+++ src/main/java/com/fellon/App.java
@@ -0,0 +1,16 @@
+package com.fellon;
+
+public class App 
+{
+public static String area = "\nплощадь ";
+    public static String perimeter = "\nпериметр ";
+    public static void main(String[] args) {
+        Circle circle = new Circle(3.0);
+        Rectangle rectangle = new Rectangle( 2.0,2.3);
+        Triangle triangle = new Triangle(3.0,3.0,3.0);
+        System.out.println("круг: " + area + circle.getArea() + perimeter + circle.getPerimeter()+"\n");
+        System.out.println("прямоугольник: " + area + rectangle.getArea() + perimeter + rectangle.getPerimeter()+"\n");
+        System.out.println("треугольник: " + area + triangle.getArea() + perimeter +  triangle.getPerimeter()+"\n");
+    }
+
+}
